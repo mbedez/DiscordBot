633bc9f4b821d11174d7aca0a4ea1ecbaf688874
mbedez <mathieu.bedez@gmail.com>
2022-01-14 00:50:06 +0100

    correction erreur listener delete guild + début sondage (poll)

--- venv/commandes/Delete.py
+++ venv/commandes/Delete.py
@@ -1,34 +1,40 @@
-import sys
 from datetime import datetime
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 
 
 class Delete(Cog):
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	@Cog.listener()
-	async def on_message(self, message):
-		# 181092186228654082 -> Tom
-		# 505198106350977024-> serveur nst "or message.channel.guild.id == 505198106350977024"
-		if message.author.id == 181092186228654082 or (message.channel.guild.id == 505198106350977024 and message.channel.id != 930914775829987388 and message.author.id != 282859044593598464):
-			messages = await message.channel.history(limit=1).flatten()
-			f = open('sortie.txt', 'a')
-			f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n{str(message.author)}\n"
-					f"{str(message.channel)}\n{str(message.content)}\n\n")
-			f.close()
-			await messages[0].delete()
-			print(f"Message écrit par {str(message.author)} à {datetime.now().strftime('%H:%M:%S')} sur "
-				  f"{str(message.channel)} : {str(message.content)}\n\n")
+    @Cog.listener()
+    async def on_message(self, message):
+        serv_nst_id = 505198106350977024
+        serv_fc_id = 464811558048890880
+        channel_hydra_id = 930914775829987388
+        probot_id = 282859044593598464
+        tom_id = 181092186228654082
 
-	@command(name='delete')
-	async def delete(self, ctx, number_of_messages: int):
-		messages = await ctx.channel.history(limit=number_of_messages + 1).flatten()
+        if message.author.id == tom_id or (message.channel.guild.id == serv_nst_id and
+                                           message.channel.id != channel_hydra_id and
+                                           message.author.id != probot_id and 1 == 2) or \
+                (message.channel.guild.id == serv_fc_id and 1 == 2):
+            messages = await message.channel.history(limit=1).flatten()
+            f = open('sortie.txt', 'a')
+            f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n{str(message.author)}\n"
+                    f"{str(message.channel)}\n{str(message.content)}\n\n")
+            f.close()
+            await messages[0].delete()
+            print(f"Message écrit par {str(message.author)} à {datetime.now().strftime('%H:%M:%S')} sur "
+                  f"{str(message.channel)} : {str(message.content)}\n\n")
 
-		for each_message in messages:
-			await each_message.delete()
+    @command(name='delete')
+    async def delete(self, ctx, number_of_messages: int):
+        messages = await ctx.channel.history(limit=number_of_messages + 1).flatten()
+
+        for each_message in messages:
+            await each_message.delete()
 
 
 def setup(bot):
-	bot.add_cog(Delete(bot))
+    bot.add_cog(Delete(bot))
--- venv/commandes/Sondage.py
+++ venv/commandes/Sondage.py
@@ -1,10 +1,29 @@
-from discord.ext.commands import Cog
+import discord
+from discord.ext.commands import Cog, command
+from discord.ext import commands
+from discord.ui import Button, View
 
 
 class Sondage(Cog):
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command(name='poll')
+    async def sondage(self, ctx: commands.Context, question, proposition1, proposition2):
+        button1 = Button(label=f"{proposition1}", style=discord.ButtonStyle.success)
+        button2 = Button(label=f"{proposition2}", style=discord.ButtonStyle.danger)
+
+        view = View()
+        view.add_item(button1)
+        view.add_item(button2)
+        await ctx.send(f"{question}", view=view)
+
+        async def button_callback(interaction):
+            await interaction.response.edit_message()
+
+        button1.callback = button_callback
+        button2.callback = button_callback
 
 
 def setup(bot):
-	bot.add_cog(Sondage(bot))
+    bot.add_cog(Sondage(bot))

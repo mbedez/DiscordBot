6679629853e75aa010cf210404367f8e62607482
Mathieu Bedez <mathieu.bedez@kyndryl.com>
2023-02-19 00:35:05 +0100

    yt_dl -> yt_dlp

--- functions/Music.py
+++ functions/Music.py
@@ -7,16 +7,20 @@ from discord.ui import Button, View
 import asyncio
 from discord.utils import V
 import logging
-import youtube_dl as ytdl
+import yt_dlp
 
 
-YTDL_OPTS = {
+yt_dlp_OPTS = {
     "default_search": "ytsearch",
-    "format": "bestaudio/best",
+    "format": "m4a/bestaudio/best",
     "audio-quality": "128K",
     "quiet": True,
     "extract_flat": "in_playlist",
-    "no_playlist": True
+    "no_playlist": True,
+    "postprocessors": [{
+        "key": "FFmpegExtractAudio",
+        "preferredcodec": "m4a"
+    }]
 }
 
 FFMPEG_BEFORE_OPTS = '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 20'
@@ -45,7 +49,7 @@ class Video:
 
     def __init__(self, url_or_search, requested_by):
         """Plays audio from (or searches for) a URL."""
-        with ytdl.YoutubeDL(YTDL_OPTS) as ydl:
+        with yt_dlp.YoutubeDL(yt_dlp_OPTS) as ydl:
             video = self._get_info(url_or_search)
             video_format = video["formats"][0]
             self.stream_url = video_format["url"]
@@ -57,7 +61,7 @@ class Video:
             self.requested_by = requested_by
 
     def _get_info(self, video_url):
-        with ytdl.YoutubeDL(YTDL_OPTS) as ydl:
+        with yt_dlp.YoutubeDL(yt_dlp_OPTS) as ydl:
             info = ydl.extract_info(video_url, download=False)
             video = None
             if "_type" in info and info["_type"] == "playlist":
@@ -229,7 +233,7 @@ class Music(commands.Cog):
             state.webhook = ctx.followup
             try:
                 video = Video(url, ctx.author)
-            except ytdl.DownloadError as e:
+            except yt_dlp.DownloadError as e:
                 logging.warn(f"Error downloading video: {e}")
                 await state.webhook.send("Une erreur est survenue pendant le téléchargement de la musique.",ephemeral=True)
                 return
@@ -243,7 +247,7 @@ class Music(commands.Cog):
                 channel = ctx.author.voice.channel
                 try:
                     video = Video(url, ctx.author)
-                except ytdl.DownloadError as e:
+                except yt_dlp.DownloadError as e:
                     await state.webhook.send("Une erreur est survenue pendant le téléchargement de la musique.",ephemeral=True)
                     return
                 client = await channel.connect()
